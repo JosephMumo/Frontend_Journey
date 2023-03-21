ac3718eaa458847b94256e9d76ab13c494f56262
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-03-21 15:52:51 +0300

    new

---
+++ Day10/index.js
@@ -0,0 +1,27 @@
+let ansEL = document.getElementById('ans-el')
+let acEl = document.getElementById('one')
+let posNeg = document.getElementById('two')
+let percent = document.getElementById('three')
+let sevEl = document.getElementById('sev-el')
+let ans;
+ansEL.innerText = ans;
+
+// function to reset display to zero
+acEl.addEventListener('click', function() {
+    ansEL.textContent = 0;
+});
+
+// function to assign negative or positive
+posNeg.addEventListener('click', function() {
+    ansEL.textContent = ans * -1;
+})
+
+// function to get the percentage
+percent.addEventListener('click', function() {
+    ansEL.textContent = ans / 100
+})
+
+sevEl.addEventListener('click', function() {
+    let num = 7;
+    ansEL.textContent += num;
+})
