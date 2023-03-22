f13be7e1ace4e06dc3f44acba6e5ce48ad1d30ce
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-03-22 13:19:43 +0300

    update

---
+++ Day14/app.js
@@ -0,0 +1,14 @@
+async function myDisplayer() {
+    let myPromise = new Promise(function(resolve, reject) {
+        setTimeout(function() {resolve("Learning Async and Await")}, 3000);
+    });
+    console.log(await myPromise)
+}
+
+myDisplayer()
+
+// async makes any function return a promise
+// the two arguments: resolve & reject are already predefined in javascript. But oftenly we don't need reject.
+// the await keyword directs the function to pause the execution and wait for a promise to be resolved before it continues.
+// myDisplayer function in the above case has a promise that has a timeout to be resolved after 3seconds.
+// the await keyword pauses the execution of myDisplayer function until the promise (myPromise) has been resolved
