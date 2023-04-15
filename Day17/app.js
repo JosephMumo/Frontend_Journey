91de23d71acb92f485fe5770b3c2bfe4c72ec57b
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-04-15 18:05:03 +0300

    hamburger

---
+++ Day17/app.js
@@ -0,0 +1,6 @@
+let hamBtn = document.querySelector('.hamburger');
+const navItm = document.querySelector('.nav-items')
+hamBtn.addEventListener('click', function() {
+    hamBtn.classList.toggle('is-active')
+    navItm.classList.toggle('is-active')
+})
